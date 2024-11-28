9fbaef3cd39a5d72974929c643421e7ed7f802a8
Bakar-creates <abubakarnadeem789@gmail.com>
2024-11-28 20:43:42 +0500

    Update app.py

--- app.py
+++ app.py
@@ -38,11 +38,11 @@ st.markdown("<h1 class='title'>🩸 Blood Bank Finder 🩸</h1>", unsafe_allow_h
 # Style for the blood bank cards with background image
 st.markdown("""
     <style>
-     st.markdown("""
+   st.markdown("""
     <style>
-        /* Change the background color of the entire app */
+        /* Set the background color of the body */
         body {
-            background-color: #F0F8FF;  /* AliceBlue color */
+            background-color: #98FB98 !important;  /* Pale Green */
         }
         /* Style for the blood bank cards */
         .blood-bank-card {
